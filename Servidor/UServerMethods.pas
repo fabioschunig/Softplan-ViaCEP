9e43a930cc9ffb73737226b464d50eedc52a4ea8
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-09 13:42:12 -0300

    Métodos do servidor para consulta por CEP e Endereços

--- Servidor/UServerMethods.pas
+++ Servidor/UServerMethods.pas
@@ -7,26 +7,96 @@ uses Classes;
 type
 {$METHODINFO ON}
   TServerMethods = class(TComponent)
-    function ConsultaCEP(const CEP: String): String;
-    function ConsultaEndereco(const UF, Cidade, Endereco: String): String;
+    function ConsultaCEP(const CEP: String; var DadosArmazenados: boolean;
+      const AtualizarDados: boolean; const Formato: String): String;
+    function ConsultaEndereco(const UF, Localidade, Logradouro: String;
+      var DadosArmazenados: boolean; const AtualizarDados: boolean;
+      const Formato: String): String;
   end;
 {$METHODINFO OFF}
 
 implementation
 
+uses
+  UDataModule, FireDAC.DApt,
+  UCEP.Intf,
+  UCEP.Repository.Intf, UCEP.Repository.FireDAC,
+  UConsulta.Intf, UConsulta.Builder.Indy,
+  UFiltros.Consulta.Intf, UFiltros.Consulta.CEP, UFiltros.Consulta.Endereco;
+
 { TServerMethods }
 
-function TServerMethods.ConsultaCEP(const CEP: String): String;
+function TServerMethods.ConsultaCEP(const CEP: String;
+  var DadosArmazenados: boolean; const AtualizarDados: boolean;
+  const Formato: String): String;
+var
+  repo: ICEPRepository;
+  aCEP: ICEPInterface;
+  aConsulta: IConsultaInterface;
+  aFiltros: IFiltrosConsultaInterface;
 begin
-  // TODO: implementar o método para buscar por CEP
-  Result := 'Consulta de CEP';
+  result := '';
+  DadosArmazenados := false;
+
+  aConsulta := TConsultaBuilderIndy.Create(Formato);
+  aFiltros := TFiltrosConsultaCEP.Create(CEP);
+  repo := TCEPRepositoryFireDAC.Create(fDataModule.FDConnection);
+
+  if not AtualizarDados then
+  begin
+    // busca na base de dados o CEP consultado previamente
+    aCEP := repo.BuscarPorCEP(CEP, DadosArmazenados);
+  end;
+
+  // solicitado atualização forçada ou não foi encontrado na base de dados
+  if AtualizarDados or (not DadosArmazenados) then
+  begin
+    aCEP := aConsulta.ConsultarSitePorCEP(aFiltros);
+    repo.Salvar(aCEP);
+  end;
+
+  result := aCEP.FormataRegistro;
 end;
 
-function TServerMethods.ConsultaEndereco(const UF, Cidade,
-  Endereco: String): String;
+function TServerMethods.ConsultaEndereco(const UF, Localidade,
+  Logradouro: String; var DadosArmazenados: boolean;
+  const AtualizarDados: boolean; const Formato: String): String;
+var
+  repo: ICEPRepository;
+  listaCEP: TArray<ICEPInterface>;
+  iCont: integer;
+  aConsulta: IConsultaInterface;
+  aFiltros: IFiltrosConsultaInterface;
 begin
-  // TODO: implementar o método para buscar por Endereço
-  Result := 'Consulta de Endereço';
+  result := '';
+  DadosArmazenados := false;
+
+  aConsulta := TConsultaBuilderIndy.Create(Formato);
+  aFiltros := TFiltrosConsultaEndereco.Create(UF, Localidade, Logradouro);
+  repo := TCEPRepositoryFireDAC.Create(fDataModule.FDConnection);
+
+  if not AtualizarDados then
+  begin
+    // busca na base de dados o CEP consultado previamente
+    listaCEP := repo.BuscarPorEndereco(UF, Localidade, Logradouro,
+      DadosArmazenados);
+  end;
+
+  // solicitado atualização forçada ou não foi encontrado na base de dados
+  if AtualizarDados or (not DadosArmazenados) then
+  begin
+    listaCEP := aConsulta.ConsultarSitePorEndereco(aFiltros);
+
+    for iCont := 0 to length(listaCEP) - 1 do
+    begin
+      repo.Salvar(listaCEP[iCont]);
+    end;
+  end;
+
+  for iCont := 0 to length(listaCEP) - 1 do
+  begin
+    result := result + listaCEP[iCont].FormataRegistro + sLineBreak;
+  end;
 end;
 
 end.
